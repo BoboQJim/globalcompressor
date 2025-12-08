4bf36287ed166afed06552b16d8ca0a99bfc5341
BoboQJim <jiboqing217@gmail.com>
2025-12-08 15:28:30 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10293661
+    "zoneId": 10293683
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
